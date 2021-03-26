f4e0b430f81ae5a3368f4f6adcab8d75c5afd665
hazem <hazem.ashraf126@gmail.com>
2021-03-26 17:22:32 +0200

    [add] Display a loader image while image is loading [add] Display a placeholder image if the URL is invalid

--- WW-Exercise-01/app/src/main/java/com/weightwatchers/ww_exercise_01/utils/databinding/Images.kt
+++ WW-Exercise-01/app/src/main/java/com/weightwatchers/ww_exercise_01/utils/databinding/Images.kt
@@ -3,10 +3,24 @@ package com.weightwatchers.ww_exercise_01.utils.databinding
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.weightwatchers.ww_exercise_01.R
 import com.weightwatchers.ww_exercise_01.utils.extensions.addBaseUrl
-
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
-    Glide.with(view).load(imageUrl?.addBaseUrl()).into(view);
+    val options: RequestOptions = RequestOptions()
+            .centerCrop()
+            .placeholder(R.drawable.image_loader)
+            .error(R.drawable.ic_baseline_food)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .priority(Priority.HIGH)
+            .dontAnimate()
+            .dontTransform()
+    Glide.with(view)
+            .load(imageUrl?.addBaseUrl())
+            .apply(options)
+            .into(view)
 }
